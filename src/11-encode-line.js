8fd6679067201fa5fcf500ace0b1d80fcd61f20a
Burbuha <burbovskaja@gmail.com>
2021-04-09 23:58:35 +0300

    feat: implement function encodeLine

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 1;
+  let result = '';
+  if (str.length === 1) {
+    return str;
+  }
+  str.split('').forEach((item, idx, arr) => {
+    if (item === arr[idx + 1]) {
+      counter++;
+    } else {
+      const count = counter === 1 ? '' : counter;
+      result += `${count}${item}`;
+      counter = 1;
+    }
+  });
+  return result;
 }
 
 module.exports = encodeLine;
